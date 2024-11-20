fb2f8c77f75bb0d9319dbc0478ebc27c2d9ef583
Ben Newland <benjamin.newland@ndsu.edu>
2024-11-20 13:46:01 -0600

    New Class Student

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Newland_Nov20.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
